b0e84f50db50fc49f2e7231409079180767b9375
Shisun <worlddominator007@gmail.com>
2022-12-29 20:44:28 +0700

    Finish  #10

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <script setup>
+import { ref } from 'vue';
 import AppHeader from './components/AppHeader.vue'
+const guess = ref('')
+const isDisable = ref(false);
+const onSubmit = () => {
+  isDisable.value = true;
+  if (guess.value === 'Rapol') {
+    alert('You are correct')
+    guess.value = ''
+    isDisable.value = false;
+    return
+  }
+  alert('Please try again in a moment !!')
+  setTimeout(() => {
+    isDisable.value = false;
+  }, 1000);
+}
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
     <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
+      <p class="text-center">Guess Who's This !</p>
       <AppHeader />
-      <p class="text-center">Good luck!</p>
+      <div class="flex gap-2">
+        <input v-model="guess" class="border border-indigo-300 px-2 py-1 rounded focus:rign-indigo-300" type="text" />
+        <button :disabled="isDisable" @click="onSubmit" class="bg-lime-500 px-2 rounded-lg text-white hover:bg-lime-700 disabled:grayscale disabled:cursor-wait">Guess</button>
+      </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
         This challenge brought to you by <a href="https://vueschool.io/" class="underline">Vue School</a>
       </h4>
     </div>
   </div>
 </template>
-
 <style>
 div {
   display: block;
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+
+const active = ref(0)
+const cssClass = ref({
+  0: 'left-0 z-10 rotate-0',
+  1: 'left-[-10px] z-0 rotate1',
+  2: 'left-[10px] z-0 rotate2',
+})
+function setActive(evt) {
+  active.value = evt.target.dataset.index
+  if (evt.target.dataset.index === '0') {
+    cssClass.value = {
+      0: 'left-0 z-10 rotate-0',
+      1: 'left-[-10px] z-0 rotate1',
+      2: 'left-[10px] z-0 rotate2',
+    }
+  } else if (evt.target.dataset.index === '1') {
+    cssClass.value = {
+      0: 'left-[-10px] z-0 rotate1',
+      1: 'left-0 z-10 rotate-0',
+      2: 'left-[10px] z-0 rotate2',
+    }
+  } else if (evt.target.dataset.index === '2') {
+    cssClass.value = {
+      0: 'left-[-10px] z-0 rotate1',
+      1: 'left-[10px] z-0 rotate2',
+      2: 'left-0 z-10 rotate-0',
+    }
+  }
+}
+</script>
 <template>
-  <header class="text-center">
-    <h2 class="text-green text-2xl">
-      <span class="bg-gray-200 px-3.5 py-1 rounded text-sm font-bold">Advent of Vue</span>
-    </h2>
-    <h1 class="text-4xl mt-2 mb-5">Secret Santa</h1>
-  </header>
+  <div class="relative flex mt-12 min-h-[270px] left-[15%]">
+    <div data-index="0" class="bg-blue-500 card" :class="[cssClass[0]]" @click.prevent="setActive">
+      <span class="card-content">I love watching anime; my favorite anime is Doctor Stone</span>
+    </div>
+    <div data-index="1" class="bg-lime-500 card" :class="[cssClass[1]]" @click.prevent="setActive">
+      <span class="card-content">I am a Frontend Developer</span>
+    </div>
+    <div data-index="2" class="bg-indigo-500 card" :class="[cssClass[2]]" @click.prevent="setActive">
+      <span class="card-content">I love to be alone</span>
+    </div>
+  </div>
 </template>
+<style scoped>
+.card {
+  @apply transition-all rounded-lg absolute w-52 h-60 p-8 flex justify-center items-center text-white;
+}
+
+.card-content {
+  @apply first-letter:capitalize first-letter:bg-clip-text first-letter:bg-gradient-to-t first-letter:from-violet-500 first-letter:to-fuchsia-500 first-letter:text-transparent first-letter:text-2xl;
+}
+
+.rotate1 {
+  transform: rotate(-10deg);
+}
+
+.rotate2 {
+  transform: rotate(10deg);
+}
+</style>
